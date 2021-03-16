c55a9e7076749876e92a0594569cff2fa8607129
Xinxin Wang <Xinxin.Wang@navihealth.com>
2021-03-16 18:38:45 -0400

    .

---
+++ src/com/xinxin/memento/Memento.java
@@ -0,0 +1,14 @@
+package com.xinxin.memento;
+
+public class Memento {
+    private final String state;
+
+    public Memento(String stateToSave) {
+        this.state = stateToSave;
+    }
+
+    public String getSavedState() {
+        return state;
+    }
+
+}
