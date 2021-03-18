8fc1716ae25f1374ad8a6331ea7adc9206bae442
Xinxin Wang <Xinxin.Wang@navihealth.com>
2021-03-17 23:28:32 -0400

    interpreter

---
+++ src/com/xinxin/interpreter/SubExpression.java
@@ -0,0 +1,15 @@
+package com.xinxin.interpreter;
+
+import java.util.HashMap;
+
+public class SubExpression extends SymbolExpression {
+
+    public SubExpression(Expression left, Expression right) {
+        super(left, right);
+    }
+
+    @Override
+    public int interpreter(HashMap<String, Integer> var) {
+        return super.left.interpreter(var) - super.right.interpreter(var);
+    }
+}
