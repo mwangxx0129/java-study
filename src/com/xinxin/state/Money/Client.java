cbfddf0d8f48184ad0eb1df9ec6ad6b3aafd975b
Xinxin Wang <mwangxx@gmail.com>
2021-03-22 10:10:38 -0400

    .

---
+++ src/com/xinxin/state/Money/Client.java
@@ -0,0 +1,20 @@
+package com.xinxin.state.Money;
+
+public class Client {
+    public static void main(String[] args) {
+        Context context = new Context();
+
+        context.setState(new PublishState());
+
+        context.acceptOrderEvent(context);
+
+        context.payOrderEvent(context);
+
+        try {
+        	context.checkFailEvent(context);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
+    }
+}
